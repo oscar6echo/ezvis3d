d7f3bbf86b9c0eed2f31c0f80bbd87a7b4bb9de2
oscar6echo <olivier.borderies@gmail.com>
2017-12-10 19:20:10 +0100

    Add custom save like ezhc

--- ezvis3d/_plot.py
+++ ezvis3d/_plot.py
@@ -10,41 +10,39 @@ from ._config import JS_LIBS_ONE, CSS_LIBS_ONE, JS_SAVE
 from .scripts import JS_JSON_PARSE_OPTION, JS_JSON_PARSE_DATA
 
 
-
-
-def html(data, options, center=False, save=False):
+def html(data, options, center=False, save=False,
+         save_name=None, save_path='saved', dated=True, notebook=True):
     """
     save=True will create a standalone HTML doc under localdir/saved (creating folfer save if necessary)
     center=True will center the plot in the output cell, otherwise left-aligned by default.
     """
-    
+
     def json_dumps(obj):
         return pd.io.json.dumps(obj)
 
     _options = dict(options)
     _data = data
 
-    def clean_function_str(key, n=10):
+    def clean_function_str(key, n=15):
         """
-        Remove new line characters in the first say 10 characters
+        Remove new line characters in the first say 15 characters
         of the value corresponding to key in dictionary _options.
         This value is a string that potentially starts with
         'function' or ' function' or '[newline]function' or '[newline]function', etc
         This cleaning makes js string parsing easier to recognize functions.
         """
         if key in _options.keys():
-            new_str = _options[key][:n].replace('\n', '').replace('\r', '') + _options[key][n:]
+            new_str = _options[key][:n].replace('\n', '')
+            new_str = new_str.replace('\r', '')
+            new_str = new_str + _options[key][n:]
             _options[key] = new_str
 
-
-    clean_function_str('tooltip') 
-    clean_function_str('xValueLabel') 
-    clean_function_str('yValueLabel') 
-    clean_function_str('zValueLabel') 
-
+    clean_function_str('tooltip')
+    clean_function_str('xValueLabel')
+    clean_function_str('yValueLabel')
+    clean_function_str('zValueLabel')
 
     chart_id = str(uuid.uuid4()).replace('-', '_')
-
 
     js_init = """
     var options = %s;
@@ -57,9 +55,8 @@ def html(data, options, center=False, save=False):
     window.dta = jQuery.extend(true, [], data);
     console.log('vis3d data accessible as dta');
     """ % (json_dumps(_options), JS_JSON_PARSE_OPTION,
-           json_dumps(_data), JS_JSON_PARSE_DATA) 
+           json_dumps(_data), JS_JSON_PARSE_DATA)
 
-    
     js_call = """
     var container = document.getElementById('%s');
     console.log(options);
@@ -67,12 +64,10 @@ def html(data, options, center=False, save=False):
     graph3d = new vis.Graph3d(container, data, options);
     """ % (chart_id)
 
-
-
     if center:
         if not 'width' in _options.keys():
             # Explicitly sets width to default value
-            _options['width'] = '400px'
+            _options['width'] = '750px'
 
         html = """
         <div id="%s" style="margin: auto; width: %s;"></div>
@@ -82,40 +77,42 @@ def html(data, options, center=False, save=False):
         <div id="%s"></div>
         """ % (chart_id)
 
-
     css = """
     <link href="%s" rel="stylesheet" type="text/css" />
     """ % (CSS_LIBS_ONE)
 
-
     js = """<script>
+    // the Jupyter notebook loads jquery.min.js  and require.js and at the top of the page
+    // then to make jquery available inside a require module
+    // the trick is http://www.manuel-strehl.de/dev/load_jquery_before_requirejs.en.html
     require(%s, function(lib) {
         window.vis = jQuery.extend(true, {}, lib);
         %s
         %s
     });
     </script>""" % (JS_LIBS_ONE, js_init, js_call)
-    
-    if save==True:
-        if not os.path.exists('saved'):
-            os.makedirs('saved')
-        with open(os.path.join('saved', 'plot_'+dt.datetime.now().strftime('%Y%m%d_%H%M%S')+'.html'), 'w') as f:
-            contents = """
-            <script src="%s"></script>
-            <script src="%s"></script>
-            %s
-            """ % (JS_SAVE[0], JS_SAVE[1], html+js)
-            f.write(contents)
-    
-    return html+css+js
 
-    
+    # save
+    js_load = ''.join(['<script src="%s"></script>' % e for e in JS_SAVE])
+    if save == True:
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        tag = save_name if save_name else 'plot'
+        dated = dt.datetime.now().strftime('_%Y%m%d_%H%M%S') if dated else ''
+
+        with open(os.path.join(save_path, tag + dated + '.html'), 'w') as f:
+            f.write(js_load + html + js)
+
+    if notebook:
+        return html + js
+    else:
+        return js_load + html + js
+
+    return html + css + js
 
 
-
-def plot(data, options, center=False, save=False):
-    contents = html(data, options, center, save)
+def plot(data, options, center=False, save=False,
+         save_name=None, save_path='saved', dated=True, notebook=True):
+    contents = html(data, options, center, save=save,
+                    save_name=save_name, save_path=save_path, dated=dated, notebook=notebook)
     return HTML(contents)
-
-  
-
--- ezvis3d/_vis3d.py
+++ ezvis3d/_vis3d.py
@@ -14,24 +14,22 @@ class Vis3d(Wrapper):
     def __init__(self):
         Wrapper.__init__(self)
 
-        #default config
+        # default config
         # self.width = '600px'
         # self.height = '600px'
 
-
-    def plot(self, df_data, center=False, save=False):
+    def plot(self, df_data, center=False, save=False,
+             save_name=None, save_path='saved', dated=True, notebook=True):
         "df_data format is a dataframe with columns x, y, z (required), and style, filter (optional)"
         data = df_data.to_json(orient='records')
         options = self.to_dict()
-        return plot(data, options, center=center, save=save)
+        return plot(data, options, center=center, save=save,
+                    save_name=save_name, save_path=save_path, dated=dated, notebook=notebook)
 
-    def html(self, df_data, center=False, save=False):
+    def html(self, df_data, center=False, save=False,
+             save_name=None, save_path='saved', dated=True, notebook=True):
         "df_data format is a dataframe with columns x, y, z (required), and style, filter (optional)"
         data = df_data.to_json(orient='records')
         options = self.to_dict()
-        return html(data, options, center=center, save=save)
-
-    # def html(self, json_data, save=False):
-    #     "json_data is a json list of records, each with fields x, y, z (required), and style, filter (optional)"
-    #     options = self.to_dict()
-    #     return html(json_data, options, save=save)
+        return html(data, options, center=center, save=save,
+                    save_name=save_name, save_path=save_path, dated=dated, notebook=notebook)
